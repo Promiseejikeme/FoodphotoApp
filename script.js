aa531f27f90f9862a313d656d80882b16ad3d28f
Promiseejikeme <103903590+Promiseejikeme@users.noreply.github.com>
2022-04-23 13:45:52 +0100

    added new files

---
+++ script.js
@@ -0,0 +1,36 @@
+//let quote = "If you believe, all things are possible"
+let x = "42" + 7;
+let y = "42" - 7;
+let coding = "cool"
+let practicing = "trendy and fun"
+window.alert(quote)
+window.alert(x)
+window.alert(y)
+console.log(coding)
+console.log(practicing)
+
+//1. 
+let x = 31 + 7;
+// enter your guess where the '0' is on the next line
+let guessOne = 38;//WRITE ANSWER =
+console.log(x);
+
+//2.
+let y = 8;
+let food = "delicious";
+// Enter your guess on the next two lines, where the 'y' and 'food' is
+console.log(8); //WRITE ANSWER = 
+console.log(delicious); //WRITE ANSWER = 
+
+//3.
+let y = (9 * 6) / 3 + 2;
+// Enter your guess on the next line, where the '0' is
+let guessTwo = 20;//WRITE ANSWER = 
+console.log(y);
+
+//4.
+let z = "We learn to code for " + "a better future, better life, and better society.";
+// Enter your guess inside the "" below
+// Be careful! The text has to match exactly.
+let guessThree = "We learn to code for a better future, better life, and better society";//WRITE ANSWER = 
+console.log(z);
